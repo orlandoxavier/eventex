47bc32a255d38e2a93d224c0f020cb8da5bc7a27
Orlando Xavier <orlandoxavier.sh@gmail.com>
2015-12-22 21:32:33 -0300

    Envio de e-mails apenas em máquina de desenvolvimento.

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 
-from django.core.mail import send_mail
 
 # Create your views here.
 def home(request):
-    mail_from = 'visitantes@eventex-orlandoxavier.herokuapp.com'
-    mail_to = 'orlandoxavier.sh@gmail.com'
     ip = request.META.get('REMOTE_ADDR')
 
-    send_mail('Novo Visitante no Eventex', 'IP: ' + ip, mail_from,
-              [mail_to], fail_silently=False)
+    if ip == '127.0.0.1':
+        mail_from = 'eventex@xavier.local'
+        mail_to = 'orlandoxavier.sh@gmail.com'
+
+        from django.core.mail import send_mail
+        send_mail('Novo Visitante no Eventex', 'IP: ' + ip, mail_from, [mail_to], fail_silently=False)
 
     return render(request, 'index.html')
