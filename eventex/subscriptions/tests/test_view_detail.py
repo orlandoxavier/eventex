6abbd3671a613f4e164610ef0174e787b6dfff20
Orlando Xavier <orlandoxavier.sh@gmail.com>
2016-01-08 11:31:25 -0300

    Thank you page

---
+++ eventex/subscriptions/tests/test_view_detail.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionDetailGet(TestCase):
+    def setUp(self):
+        self.obj = Subscription.objects.create(
+            name='Orlando Xavier',
+            cpf='12345678901',
+            email='cadastros@orlandoxavier.net',
+            phone='83-99619-8699',
+        )
+        self.resp = self.client.get('/inscricao/{}/'.format(self.obj.pk))
+
+    def test_get(self):
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp,
+                                'subscriptions/subscription_detail.html')
+
+    def test_context(self):
+        subscription = self.resp.context['subscription']
+        self.assertIsInstance(subscription, Subscription)
+
+    def test_html(self):
+        contents = (
+            self.obj.name, self.obj.cpf,
+            self.obj.email, self.obj.phone
+        )
+        with self.subTest():
+            for expected in contents:
+                self.assertContains(self.resp, expected)
+
+
+class SubscriptionDetailNotFound(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/inscricao/0/')
+
+    def test_not_found(self):
+        self.assertEqual(404, self.resp.status_code)
